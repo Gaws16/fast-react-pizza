286ad0527f86a73f608416defcc5615f22b9a800
Gaws16 <gaws@mail.bg>
2025-08-23 23:11:48 +0300

    fix conflicts

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
-<<<<<<< HEAD
+
 import cartReducer from "./features/cart/cartSlice";
 
 const store = configureStore({
@@ -10,11 +10,3 @@ const store = configureStore({
   },
 });
 export default store;
-=======
-const store = configureStore({
-    reducer:{
-        user: userReducer,
-    }
-});
-export default store;
->>>>>>> 5f458fb32ad4aa7656260e17dc4726dffbfd9e33
