46201e2732951ff9c3d52bd04b3ba4c305705686
Gaws16 <gaws@mail.bg>
2025-08-23 23:05:18 +0300

    feat: Add cart slice and configure store for cart management

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,56 @@
+const initialState = {
+  //cart: [],
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Pepperoni Pizza",
+      quantity: 2,
+      unitPrice: 12.99,
+      totalPrice: 25.98,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      // Payload is a new item to add to the cart
+      state.cart.push(action.payload);
+    },
+    deleteItem: (state, action) => {
+      // Payload is the pizzaId of the item to delete
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity: (state, action) => {
+      // Payload is the pizzaId of the item to increase
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      if (item) {
+        item.quantity += 1;
+        item.totalPrice = item.unitPrice * item.quantity;
+      }
+    },
+    decreaseItemQuantity: (state, action) => {
+      // Payload is the pizzaId of the item to decrease
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+        item.totalPrice = item.unitPrice * item.quantity;
+      }
+    },
+    clearCart: (state) => {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  deleteItem,
+  addItem,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
