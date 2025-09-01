4ef12349c8ef0b6185395e603d93c7a6cbcf5a09
Gaws16 <gaws@mail.bg>
2025-09-01 21:05:27 +0300

    feat: Implement item quantity update functionality in UpdateItemQuantity component

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,10 +1,24 @@
+import { useDispatch } from "react-redux";
 import Button from "../ui/Button";
+import { decreaseItemQuantity, increaseItemQuantity } from "./cartSlice";
 
-function UpdateItemQuantity() {
+function UpdateItemQuantity({ pizzaId }) {
+  const dispatch = useDispatch();
+
   return (
     <div className="flex gap-1 md:gap-1">
-      <Button type="round">-</Button>
-      <Button type="round">+</Button>
+      <Button
+        onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
+        type="round"
+      >
+        -
+      </Button>
+      <Button
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+        type="round"
+      >
+        +
+      </Button>
     </div>
   );
 }
