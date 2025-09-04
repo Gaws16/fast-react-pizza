d1a52c122e8bb3300562ee533b87e16a40843f1c
Gaws16 <gaws@mail.bg>
2025-09-04 19:24:08 +0300

    feat: Add redux-persist for state persistence and update store configuration

--- src/store.js
+++ src/store.js
@@ -1,12 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
 
 import cartReducer from "./features/cart/cartSlice";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage: localStorage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-    cart: cartReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Disable serializable check for redux-persist
+    }),
 });
-export default store;
+export const persistor = persistStore(store);
